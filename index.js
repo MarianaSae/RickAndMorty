ad9aa36f9b899a92c3266d5ff2c1485a38e6e9f6
MarianaSae <ysaenz0510@gmail.com>
2026-03-21 18:20:30 -0600

    se agregan los comentarios

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function getCharacters(done){
-    const results = fetch ("https://rickandmortyapi.com/api/character");
+function getCharacters(done){//definicion de funcion y parametro
+    const results = fetch ("https://rickandmortyapi.com/api/character");//fetch para la peticion, lo guarda en result
 
     results
-    .then(response => response.json())
+    .then(response => response.json())//obtiene la respuesta del servidor y lo hace a formato json
     .then(data =>{
-        done(data)
+        done(data)//recibe y usa los datos 
     });
 }
 
-    getCharacters(data =>{
-        console.log(data);
+    getCharacters(data =>{//funcion
+        console.log(data);//imprime los datos obtenidos
     });
